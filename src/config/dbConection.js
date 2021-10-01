f616501510fd1f28bc59079a3a6626d1f156d563
Rafael Nascimento <rafaelradan76@gmail.com>
2021-10-01 10:28:41 -0300

    Connectig to DB in cloud

--- src/config/dbConection.js
+++ src/config/dbConection.js
@@ -3,14 +3,14 @@ import {config} from 'dotenv'
 
 config()
 
-//Conexão com db local
+/* //Conexão com db local
 export const sequelize = new Sequelize('db_crud_myfirstapp', 'root', '202817', {
   host: 'localhost',
   dialect: 'mysql' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-})
+//}) */
 
 //Conexão com bd na nuvem
-/* export const sequelize = new Sequelize({
+export const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -18,13 +18,13 @@ export const sequelize = new Sequelize('db_crud_myfirstapp', 'root', '202817', {
   database: process.env.DB_NAME,
   dialect: 'mysql',
   logging: false,
-  }); */
+  });
 
 
   async function connectDatabase(){
     try {
       sequelize.authenticate()
-      sequelize.sync({ force: false })
+      sequelize.sync({ force: true })
       console.log('connected to databese')
       
     } catch (error) {
